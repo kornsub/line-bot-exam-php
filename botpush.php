3a731ce73a54c595765caf69ca55416b4b15bbf9
kornsub <kornsub.s@gmail.com>
2018-06-16 11:53:16 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,9 +1,10 @@
 <?php
 
 require "vendor/autoload.php";
-
+print_r($_POST);
 $data = json_decode($_POST, true);
 
+echo $_POST[0];
 print_r($data);
 
 $access_token = 'ip/pRN4xHdsvZP7E+9BFQNrgNz1cBbwU+92eHsKjWKauP14oprQF9K57kxbaMTxLVTvJtAWrYZNVLe1V1ikZ+9NRWXc9u6HBgf/RBrmmXkq3cNbOrtRILf9ogEUQ2yzjDyoFJTORu5g4O0g15jP6rAdB04t89/1O/w1cDnyilFU=';
