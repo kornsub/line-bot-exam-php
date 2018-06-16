6a27c3c0be0b1c25b27dd13f801ee17beecb97bf
kornsub <kornsub.s@gmail.com>
2018-06-16 12:06:35 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -2,17 +2,12 @@
 
 require "vendor/autoload.php";
 
-var_dump($_POST);
-$data = json_decode($_POST,true);
-
-var_dump($data);
-
 $access_token = 'ip/pRN4xHdsvZP7E+9BFQNrgNz1cBbwU+92eHsKjWKauP14oprQF9K57kxbaMTxLVTvJtAWrYZNVLe1V1ikZ+9NRWXc9u6HBgf/RBrmmXkq3cNbOrtRILf9ogEUQ2yzjDyoFJTORu5g4O0g15jP6rAdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = 'd092e4a4d7760d4c5f62b396f7b8e5dd';
 
-$pushID = $data['id'];
-$textmsg = $data['text'];
+$pushID = $_POST['id'];
+$textmsg = $_POST['text'];
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
